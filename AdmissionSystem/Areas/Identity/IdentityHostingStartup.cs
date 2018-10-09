83c65fd5580e470200cd68e3d8b56efbeed9817b
Ruchit Patel <rpruchitpatel14@gmail.com>
2018-10-09 16:52:34 +0530

    identity crisis (lol)

---
+++ AdmissionSystem/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using AdmissionSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AdmissionSystem.Areas.Identity.IdentityHostingStartup))]
+namespace AdmissionSystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AdmissionSystemIdentityDbContext>(options =>
+                                                                        options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AdmissionSystemIdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<AdmissionSystemIdentityDbContext>();
+            });
+        }
+    }
+}
